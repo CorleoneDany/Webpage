cfabd14c2a96684c1abe8b654a8de7ca3c48dd44
Corleone <corleonedany@live.fr>
2021-07-19 02:24:00 +0200

    Suppression dans l'html
    Ajout de commentaires dans le js
    
    Ajout de la methode update_Body()

--- request.js
+++ request.js
@@ -1,4 +1,5 @@
 const requestURL = "http://localhost:8000/api/v1"
+const movies = {}
 
 function get_Data() {
 
@@ -16,22 +17,39 @@ function get_Data() {
 
     // boucle pour chaque film dans result -> modifier le html des squares dynamiquement -> dataset < data- >
 
-    // action / animation / fantasy / horror genre=<name>
 
 }
 
 function get_Best_Movie() {
     fetch(requestURL + "/titles/?sort_by=-imdb_score")
         .then(response => {
-            return response.json()
+            return response.json()[results][0]
             // Prendre seulement le premier resultat de results et ok
-        })
+        }
+            // <div id="Meilleur_Film">
+
+            //     <div class="square">
+
+            //     </div>
+
+            // </div>
+
+        )
 }
 
 function get_Movies_By_Type(type) {
-    fetch(requestURL + "/titles/?sort_by=-imdb_score&genre=" + type)
+    fetch(requestURL + "/titles/?sort_by=-imdb_score&genre=" + type.charAt(0).toUpperCase())
         .then(response => {
-            return response.json()
-            // Prendre seulement les 7 premiers résultats dans results puis les insérer dans la div avec la data = a type
+            movies[type] = response.json()[results][0, 6]
+            // Prendre seulement les 7 premiers résultats dans results puis les insérer dans la const movies
         })
+}
+
+function update_Body() {
+    let body = document.getElementById("Container")
+    for (type in movies) {
+        for (movie_info in type) {
+            // Ajouter l'html dans le body 
+        }
+    }
 }
